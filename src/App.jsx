f41f79366d91d42d2a83a868c2b08e685517c3b1
Jatin14katyal <katyalsaab496@gmail.com>
2026-02-04 00:52:50 +0530

    created components and worked on Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Layout from './Components/Layout'
+import Home from './Components/Home'
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 const App=()=>{
   return(
-    <div>App JSX thanks </div>
+    <Router>
+      <Layout />
+      
+      <Routes>
+        <Route path='/' index element={<Home/>} />
+        <Route path='/login' index element={<Login/>} />
+        <Route path='/register' index element={<Register/>} />
+      </Routes>
+    </Router>
   )
 }
 
