ed81786561fba8928ba8ab083d4ac00fd4112b54
Jatin14katyal <katyalsaab496@gmail.com>
2026-02-10 01:42:06 +0530

    add features in the app

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './Hero'
 import FeaturedCars from './FeaturedCars'
 import VideoSection from './VideoSection'
+import Features from './Features'
 
 const Home = () => {
   return <>
   <Hero/>
   <FeaturedCars/>
   <VideoSection/>
+  <Features />
   </>
 }
 
