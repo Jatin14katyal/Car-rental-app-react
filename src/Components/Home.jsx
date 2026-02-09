a640bd3367ea7c67b8fbfae49ebff75b66dfbf4a
Jatin14katyal <katyalsaab496@gmail.com>
2026-02-10 00:02:30 +0530

    Added Video secton in the app

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from './Hero'
 import FeaturedCars from './FeaturedCars'
-
+import VideoSection from './VideoSection'
 
 const Home = () => {
   return <>
   <Hero/>
   <FeaturedCars/>
+  <VideoSection/>
   </>
 }
 
