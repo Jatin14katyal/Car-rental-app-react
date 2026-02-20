20cd746bf5c271fc265b6b75cc77851cbf331fe2
Jatin14katyal <katyalsaab496@gmail.com>
2026-02-20 15:53:13 +0530

    Rename

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -23,7 +23,7 @@ const Nav = () => {
             className="flex items-center space-x-2 text-gray-800"
           >
             <Car className="h-8 w-8 " />
-            <span className="text-xl font-bold">AutoRent</span>
+            <span className="text-xl font-bold">CarDekho</span>
           </button>
 
           <div className="hidden md:flex items-center space-x-8">
